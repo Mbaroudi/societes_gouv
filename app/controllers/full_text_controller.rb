748778ee3902ef4c2188ff72279c202d2e0ff04f
JC <blondel.jc@gmail.com>
2017-01-24 14:18:49 +0100

    Replicate siret controller to get fuzzy_search from textacular on new full_text controller (scope in model: only searching for siren, siret, nom_raison_sociale)

---
+++ app/controllers/full_text_controller.rb
@@ -0,0 +1,15 @@
+class FullTextController < ApplicationController
+  def show
+    # NICE SEGURIDAD
+    r = Entreprise.fuzzy_search(params[:id])
+    if r.nil?
+      render json: { message: 'no results found' }, status: 404
+    else
+      render json: { etablissement: r.all }, status: 200
+    end
+  end
+
+  def full_text_params
+    params.permit(:id)
+  end
+end
