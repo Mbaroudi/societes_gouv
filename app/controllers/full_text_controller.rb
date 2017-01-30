526f907172fe8aaca33c36d95d94b17a713566a1
JC <blondel.jc@gmail.com>
2017-01-30 12:01:58 +0100

    Renaming Entreprise to Etablissement in full_text_controller (previous commit forced)

--- app/controllers/full_text_controller.rb
+++ app/controllers/full_text_controller.rb
@@ -1,7 +1,7 @@
 class FullTextController < ApplicationController
   def show
     # NICE SEGURIDAD
-    r = Entreprise.fuzzy_search(params[:id]).limit(10)
+    r = Etablissement.fuzzy_search(params[:id]).limit(10)
     if r.nil?
       render json: { message: 'no results found' }, status: 404
     else
